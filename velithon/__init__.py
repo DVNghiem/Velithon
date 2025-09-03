b976c6f030d6c1bf63944722864180919ad81652
Dang Van Nghiem <vannghiem848@gmai.com>
2025-09-03 23:23:19 +0700

    bump: Update version to 0.6.5 across project files

--- velithon/__init__.py
+++ velithon/__init__.py
@@ -3,7 +3,7 @@
 Velithon is a modern, fast (high-performance), web framework for building APIs
 """
 
-__version__ = '0.6.4'
+__version__ = '0.6.5'
 
 # Core application
 from .application import Velithon
