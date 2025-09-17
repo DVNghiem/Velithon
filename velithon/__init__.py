0d3f195cabd7922265e5194e7e5ba73f17cee608
Dang Van Nghiem <vanghiem848@gmail.com>
2025-09-17 09:13:17 +0700

    bump: Update version to 0.6.9 in Cargo.lock, Cargo.toml, pyproject.toml, and __init__.py

--- velithon/__init__.py
+++ velithon/__init__.py
@@ -3,7 +3,7 @@
 Velithon is a modern, fast (high-performance), web framework for building APIs
 """
 
-__version__ = '0.6.8'
+__version__ = '0.6.9'
 
 # Core application
 from .application import Velithon
